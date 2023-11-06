9661916933f16f97dd01c0f7f9535e4c7118c7eb
sheezaHadayat <sheezahadayat@gmail.com>
2023-11-06 19:31:07 +0500

    tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '450px',
+        // => @media (min-width: 576px) { ... }
+  
+        'md': '960px',
+        // => @media (min-width: 960px) { ... }
+  
+        'lg': '1440px',
+        // => @media (min-width: 1440px) { ... }
+      },
+     
+    },
   },
   plugins: [],
 }
